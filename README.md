24ee12617cd0b17a887ab7c92d75a95d74169e22
v-giorgio <vitorlcardoso17@gmail.com>
2024-07-16 22:02:32 -0300

    feat: add main file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ray tracer (Python)
+
+#### Aplicação para geração de imagens usando técnicas de Álgebra Linear e iluminação de Blinn-Phong.
+
+#### Exemplo:
+
+![Imagem com 3 esferas](/asset/another_sample.png)
--- ray_tracer.py
+++ ray_tracer.py
@@ -6,21 +6,20 @@ from scene import Scene
 
 file_name = 'another_sample'
 
+# Altere a posição do espectador aqui:
 camera = Vector(0, 0, 1)
-# light = Light(Vector(0, 0, 1), np.array([1, 1, 1]), np.array([1, 1, 1]), np.array([1, 1, 1]))
-# objects = [
-#     Sphere(Vector(0, 0, -1), 0.7, np.array([1, 0.5, 0]), np.array([1, 0.5, 0]), np.array([1, 1, 1]), 100, 0.5),  # Orange sphere
-#     Sphere(Vector(0, -9000, 0), 9000 - 0.7, np.array([0.1, 0.05, 0]), np.array([0.15, 0.07, 0.03]), np.array([1, 1, 1]), 100, 0.5)  # Deep dark brown field
-# ]
 
+# Altere a posição da fonte luminosa aqui:
 light = Light(Vector(5, 5, 5), np.array([1, 1, 1]), np.array([1, 1, 1]), np.array([1, 1, 1]))
+
+# Adicione objetos na imagem aqui:
 objects = [
     Sphere(Vector(-0.2, 0, -1), 0.7, np.array([1, 0.5, 0]), np.array([0.1, 0, 0]), np.array([1, 1, 1]), 100, 0.5),
     Sphere(Vector(0.1, -0.3, 0), 0.1, np.array([0.1, 0, 0.1]), np.array([0.7, 0, 0.7]), np.array([1, 1, 1]), 100, 0.5),
     Sphere(Vector(-0.3, 0, 0), 0.15, np.array([0, 0.1, 0]), np.array([0, 0.6, 0]), np.array([1, 1, 1]), 100, 0.5),
     Sphere(Vector(0.1, -9000, 0), 9000 - 0.7, np.array([0.1, 0.1, 0.1]), np.array([0.6, 0.6, 0.6]), np.array([1, 1, 1]), 100, 0.5)
 ]
-
 scene = Scene(camera, objects, light)
 
+# Altere aqui para alterar as dimensões da imagem:
 scene.render(1000, 1000, 3, file_name)
